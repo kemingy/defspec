253b2a16c24a3ee921ce49e64c564895a876468d
Keming <kemingy94@gmail.com>
2024-01-20 18:21:25 +0800

    init project
    Signed-off-by: Keming <kemingy94@gmail.com>

---
+++ Makefile
@@ -0,0 +1,20 @@
+PY_SOURCE=reapi tests examples
+
+dev:
+	@pip install -e .[dev]
+
+lint:
+	@ruff check ${PY_SOURCE}
+
+format:
+	@ruff check --fix ${PY_SOURCE}
+	@ruff format ${PY_SOURCE}
+
+clean:
+	@-rm -rf dist build */__pycache__ *.egg-info argstruct/__version__.py
+
+build:
+	@python -m build
+
+test:
+	@pytest -v tests
---
+++ reapi/template.py
@@ -0,0 +1,103 @@
+from enum import Enum
+
+SwaggerTemplate = """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="description"
+      content="SwaggerUI"
+    />
+    <title>SwaggerUI</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+  </head>
+  <body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5.0.0/swagger-ui-bundle.js" crossorigin></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.0.0/swagger-ui-standalone-preset.js" crossorigin></script>
+  <script>
+    window.onload = () => {{
+      window.ui = SwaggerUIBundle({{
+        url: '{spec_url}',
+        dom_id: '#swagger-ui',
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout",
+      }});
+    }};
+  </script>
+  </body>
+</html>
+"""
+
+ReDocTemplate = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ReDoc</title>
+        <!-- needed for adaptive design -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href=
+        "https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700"
+        rel="stylesheet">
+
+        <!--
+        ReDoc doesn't change outer page styles
+        -->
+        <style>
+        body {{
+            margin: 0;
+            padding: 0;
+        }}
+        </style>
+    </head>
+    <body>
+        <redoc spec-url='{spec_url}'></redoc>
+        <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"> </script>
+    </body>
+</html>
+"""
+
+ScalarTemplate = """
+<!doctype html>
+<html>
+  <head>
+    <title>API Reference</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1" />
+    <style>
+      body {{
+        margin: 0;
+      }}
+    </style>
+  </head>
+  <body>
+    <script
+      id="api-reference"
+      data-url="{spec_url}">
+    </script>
+    <script>
+      var configuration = {{
+        theme: 'purple',
+      }}
+
+      var apiReference = document.getElementById('api-reference')
+      apiReference.dataset.configuration = JSON.stringify(configuration)
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+  </body>
+</html>
+"""
+
+
+class RenderTemplate(Enum):
+    SWAGGER = SwaggerTemplate
+    REDOC = ReDocTemplate
+    SCALAR = ScalarTemplate
